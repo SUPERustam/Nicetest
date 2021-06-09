91eeb1b6331c92dcb88982a101a1c63659269de9
SUPERustam <498rustam@gmail.com>
2021-06-09 20:02:13 +0300

    add new clear and test flags

--- modules.py
+++ modules.py
@@ -1,33 +1,83 @@
-# rich, argparse, parsing website
+# rich, parsing website
 import os
 from pprint import pprint
 import json
 import subprocess as sp
 
-# TODO:
-def run(file: str, test: str):
+
+def clear_func(clear_var: list, settings: dict) -> None:
+    if not clear_var:
+        if input("Are you really want clear all tests and results? [Y/n] ")[0] == 'Y':
+            from shutil import rmtree
+            rmtree('Nicetest/data')
+            os.mkdir('Nicetest/data')
+            with open('Nicetest/data/settings.json', 'w') as f:
+                f.write(json.dumps({}))
+            print('Done')
+        return
+    if input(f"Are you really want clear tests and results from {', '.join(clear_var)}? [Y/n] ") != 'Y':
+        return
+    if not settings:
+        with open('Nicetest/data/settings.json') as f:
+            f = f.read()
+        settings = json.loads(f)
+    for file in clear_var:
+        if file in settings:
+            os.remove(settings[file])
+            del settings[file]
+            print(f"Removed tests & results from {file}")
+        else:
+            print(f'Not found tests & results from {file}')
+
+    file = json.dumps(settings)
+    with open('Nicetest/data/settings.json', 'w', encoding='utf-8') as f:
+        f.write(file)
+
+
+def test_func(test_var: list, file='', dct=None) -> dict:
     prog = ['./test.out']
-    test_name = f"data/test_{file[file.rfind('/') + 1:file.rfind('.')]}.json"
-    if os.path.exists(test):
-        with open(test) as f:
-            f = f.read()
-    else:
-        with open('data/template_test.json') as f:
-            f = f.read()
-    tests = json.loads(f)
+    if file:
+        compiling = sp.run(['g++', file, '-o', 'test.out'], stdout=sp.PIPE)
+        for i, test in enumerate(dct["tests"]):
+            oupt = sp.run(prog, stdout=sp.PIPE, input=test['input'].encode('utf-8')).stdout.decode("utf-8")
+            rslt = "OK" if oupt == test['answer'] else "WA"
+            dct["results"][i]["output"].append(oupt)
+            dct["results"][i]["result"].append(rslt)
+        print(f"Done {file}")
+        return dct
+    with open('Nicetest/data/settings.json') as f:
+        f = f.read()
+    settings = json.loads(f)
+    for file in test_var:
+        try:
+            with open(settings[file]) as f:
+                f = f.read()
+        except KeyError:  # todo: error, not found file in settings.json
+            return
+        dct = json.loads(f)
+        compiling = sp.run(['g++', file, '-o', 'test.out'], stdout=sp.PIPE)
+        for i, test in enumerate(dct["tests"]):
+            oupt = sp.run(prog, stdout=sp.PIPE, input=test['input'].encode('utf-8')).stdout.decode("utf-8")
+            rslt = "OK" if oupt == test['answer'] else "WA"
+            dct["results"][i]["output"].append(oupt)
+            dct["results"][i]["result"].append(rslt)
 
-    compiling = sp.run(['g++', file, '-o', 'test.out'], stdout=sp.PIPE)
-    for test in tests:
-        inpt = test['input'].encode('utf-8')
-        oupt = sp.run(prog, stdout=sp.PIPE, input=inpt).stdout.decode('utf-8')
+        dct = json.dumps(dct, ensure_ascii=False)
+        with open(settings[file], 'w', encoding='utf8') as f:
+            f.write(dct)
+        print(f"Done {file}")
 
-        if not os.path.exists(test_name):
-            test["output"], test["result"] = [], []
-        test['output'].append(oupt)
-        rslt = "OK" if oupt == test['answer'] else "WA"
-        test['result'].append(rslt)
 
-    tests = json.dumps(tests)
-    with open(test_name, 'w', encoding='utf8') as f:
-        f.write(tests)
-    yield f'Done {file}\n'
+def add_func(add_var: list, dct: dict, count: list) -> dict:
+    for i in range(0, len(add_var), 3):
+        dct2 = {
+            "input": add_var[i],
+            "answer": add_var[i + 1],
+            "comment": add_var[i + 2]
+        }
+        dct["tests"].append(dct2)
+        dct["results"].append({
+            "output": count,
+            "result": count
+        })
+    return dct
--- nicetest.py
+++ nicetest.py
@@ -1,39 +1,45 @@
-# rich, argparse, parsing website
+# rich, parsing website
 import os
 from pprint import pprint
 import json
-import subprocess as sp
 import argparse
 
-from modules import run
+from modules import test_func, add_func, clear_func
 
 parser = argparse.ArgumentParser()
-
-parser.add_argument('-r', '--run', help='run script from path or run default script without arguments', nargs='?')
-parser.add_argument('-c', '--connect', help='connect tests to your scripts', nargs='+')
+parser.add_argument('-t', '--test', help="test your files: cppfile0.cpp cppfile1.cpp...", nargs='+',
+                    metavar='')
+parser.add_argument('-c', '--connect', help='connect tests to your scripts: cppfile0.cpp cppfile1.cpp...', nargs='+',
+                    metavar='')
 parser.add_argument('-a', '--add',
-                    help='''add test: "input0" "answer0" "comment0" "input1" "answer1" "comment1" etc.''',
-                    nargs='+')  # TODO:
+                    help='''add tests: "input0" "answer0" "comment0" "input1" "answer1" "comment1"...''',
+                    nargs='+', metavar='')
+parser.add_argument('--clear',
+                    help="""clear all tests and results from files (without arguments: from all files):
+                     "cppfile0.cpp" "cppfile1.cpp"...""",
+                    metavar='', nargs='*')
 
 args = parser.parse_args()
-run_var = args.run
+test_var = args.test
 connect_var = args.connect
 add_var = args.add
+clear_var = args.clear
+settings = {}
 
+# output: settings
 if connect_var:
     if not add_var:  # todo: error you should input values
         pass
     elif len(add_var) % 3 != 0:  # todo: error you should write "input", "answer", "comment" to every test
         pass
-
-    with open('data/settings.json') as f:
+    with open('Nicetest/data/settings.json') as f:
         f = f.read()
     settings = json.loads(f)
     for item in connect_var:
         count = 0  # count of tests
 
         if item not in settings:
-            settings[item] = f"data/test_{item[item.rfind('/') + 1:item.rfind('.')]}.json"
+            settings[item] = f"Nicetest/data/test_{item[item.rfind('/') + 1:item.rfind('.')]}.json"
             dct = {
                 "tests": [],
                 "results": []
@@ -44,41 +50,22 @@ if connect_var:
             dct = json.loads(f)
             count = len(dct["results"][0]['result'])
 
+        # settings[item]: dct == file: dict from test.json
         if add_var:
-            count *= ['']
-            for i in range(0, len(add_var), 3):
-                dct2 = {
-                    "input": add_var[i],
-                    "answer": add_var[i + 1],
-                    "comment": add_var[i + 2]
-                }
-                dct["tests"].append(dct2)
-                dct["results"].append({
-                    "output": count,
-                    "result": count
-                })
+            dct = add_func(add_var, dct, count * [''])
+        if test_var:
+            dct = test_func([], item, dct)
 
-        dct = json.dumps(dct)
+        dct = json.dumps(dct, ensure_ascii=False)
         with open(settings[item], 'w', encoding='utf8') as f:
             f.write(dct)
 
-    settings = json.dumps(settings)
-    with open('data/settings.json', 'w', encoding='utf8') as f:
-        f.write(settings)
+    file = json.dumps(settings)
+    with open('Nicetest/data/settings.json', 'w', encoding='utf8') as f:
+        f.write(file)
 
-if run_var:
-    with open('data/settings.json') as f:
-        f = f.read()
-    settings = json.loads(f)
-    settings2 = settings.copy()
+elif test_var:
+    test_func(test_var)
 
-    if run_var == '':
-        ky = next(iter(settings))
-        print(*run(ky, settings[ky]))
-    else:
-        settings.setdefault(run_var, '')
-        if settings2 != settings:
-            file = json.dumps(settings)
-            with open('data/settings.json', 'w', encoding='utf8') as f:
-                f.write(file)
-        print(*run(run_var, settings[run_var]))
+if clear_var == [] or clear_var:
+    clear_func(clear_var, settings)
